ca187592c7662f29c8dc143d7f59fafb1f12779e
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-08-03 15:26:56 -1000

    added favoriteUsers array to user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true, min: 8 },
-    favoriteItems: [String] 
+    favoriteItems: [String], 
+    favoriteUsers: [String]
 }, { timestamps: true });
 
 
